ac14ba380bc259232af03a27dfd5a6d7b7be1b6f
unknown <maqeel191@gmail.com>
2024-12-03 11:29:45 -0800

    implementationapiIntegration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [joke, setJoke] = useState([])
+  useEffect(()=>{
+   fetch('http://localhost:3000/joke')  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })  .then((data) => {
+    setJoke(data);
+    // setLoading(false);
+  }).catch((error)=>{
+     console.log(error)
+   })
+  },[])
   return (
    <>
    Full stack application
+{
+  joke.map((item,index)=>{
+    return(
+    <div key={item.id}>
+      <h2>{item?.title}</h2>
+      <p>{item?.content}</p>
+    </div>)
+  })
+}
    </>
   )
 }
