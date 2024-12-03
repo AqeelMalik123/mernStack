7ddeec1acee1709b4f6d26a64394a83848e0f19d
unknown <maqeel191@gmail.com>
2024-12-03 10:17:53 -0800

    jokeroutes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,27 @@ const app= express();
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
+app.get('/joke',(req,res)=>{
+    const joke=[{
+        id:1,
+        content:"joke 1",
+        title:"this is first joke"
+    },
+    {
+        id:2,
+        content:"joke 2",
+        title:"this is second joke"
+    },
+    {
+        id:3,
+        content:"joke 3",
+        title:"this is third joke"
+    }               
+
+
+]
+    res.send(joke)
+})
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`server at https://localhost:${port}`)
