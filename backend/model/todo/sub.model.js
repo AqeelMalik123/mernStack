eb31d585204812e4e6b17d8dc6d5b176d4d8bfb0
unknown <maqeel191@gmail.com>
2024-12-06 11:47:12 -0800

    arrayOfSubTodoModel

--- backend/model/todo/sub.model.js
+++ backend/model/todo/sub.model.js
@@ -1,33 +1,4 @@
 import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:8,
-    }
-},{
-    timestamps:true,
-    versionKey:false,
-    toJSON:{
-        virtuals:true,
-        transform:(doc,ret)=>{
-            delete ret.__v
-            return ret
-        }
-    }
- 
-})
-export const User=mongoose.model('User',UserSchema)
+const SubTodoSchema= new mongoose.Schema({},{timestamps:true})
+export const  SubTodo= mongoose.model('SubTodo',SubTodoSchema)
--- backend/model/todo/todo.model.js
+++ backend/model/todo/todo.model.js
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
 
 const TodoSchema= new mongoose.Schema({
-
+ content:{
+    type:String,
+    required:true
+ },
+ completed:{
+    type:Boolean,
+    default:false
+ },
+ createdAt:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+ },
+ subTodo:[
+{type:mongoose.Schema.Types.ObjectId,
+ref:"SubTodo"}
+ ]
 },{timestamps:true})
 export const Todo=mongoose.model('Todo',TodoSchema);
