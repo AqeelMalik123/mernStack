af097a5c64b5c2494ea0f6a7bd3c9d91891b0978
unknown <maqeel191@gmail.com>
2024-12-06 11:51:26 -0800

    datamodelling

--- backend/model/todo/sub.model.js
+++ backend/model/todo/sub.model.js
@@ -1,4 +1,17 @@
 import mongoose from 'mongoose'
 
-const SubTodoSchema= new mongoose.Schema({},{timestamps:true})
+const SubTodoSchema= new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    createdAt:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
 export const  SubTodo= mongoose.model('SubTodo',SubTodoSchema)
