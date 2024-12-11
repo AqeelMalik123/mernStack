04e1d6f0503bc884eb4292b1c1ec987eaddffe2c
unknown <maqeel191@gmail.com>
2024-12-11 09:53:25 -0800

    productSchema

--- backend/model/ecommerce/user.model.js
+++ backend/model/ecommerce/user.model.js
@@ -16,6 +16,8 @@ const userSchema= new mongoose.Schema({
     password:{
         type:String,
         required:true,
-        // minlength:6
+       
     }
 },{timestamps:true})
+
+export const User=mongoose.model('User',userSchema)
