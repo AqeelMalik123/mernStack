25d5a248de21e646b3b6c4d84237e4c9d65ef610
unknown <maqeel191@gmail.com>
2024-12-11 10:16:01 -0800

    orderModels

--- backend/model/ecommerce/product.model.js
+++ backend/model/ecommerce/product.model.js
@@ -1,4 +1,33 @@
 import mongoose from "mongoose";
 
-const productSchema= new mongoose.Schema({},{timestamps:true});
+const productSchema= new mongoose.Schema({
+    description:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    productImage:{type:String, required:true
+},
+price:{
+    type:Number,
+    required:true
+},
+stock:{
+    default:0,
+    type:Number
+},
+category:{
+    type:mongoose.Schema.Types.ObjectId,
+//   require:true,
+  ref:"Category"
+},
+owner:{
+ type:mongoose.Schema.Types.ObjectId,
+ ref:"User"
+}
+
+},{timestamps:true});
 export const Product= mongoose.model('Product',productSchema)
